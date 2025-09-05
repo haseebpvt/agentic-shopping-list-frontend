76b9b06f09eb70ae2e61ef2f37d6ddd77699119d
haseebpvt <haseebpvt@gmail.com>
2025-09-05 16:04:09 +0530

    Add `ApiService` abstract class with `getProductSuggestion` method

---
+++ lib/data/api_service.dart
@@ -0,0 +1,8 @@
+import 'package:advanced_shopping_list_frontend/data/model/product_suggestion/product_suggestion.dart';
+
+abstract class ApiService {
+  Future<ProductSuggestion> getProductSuggestion(
+    String userId,
+    String filePath,
+  );
+}
