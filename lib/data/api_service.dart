d6346ee2388242479ed24c367ec0e54addd0a36c
haseebpvt <haseebpvt@gmail.com>
2025-09-05 16:17:44 +0530

    Implement `getProductSuggestion` method in `ApiServiceImpl`
    The `getProductSuggestion` method in `ApiServiceImpl` now sends a POST request to the "/get_product_recommendation" endpoint with the user ID and the image file as form data. It returns the response data as a `ProductSuggestion` object.
    
    The `ApiServiceImpl` constructor now takes a `Dio` instance as a parameter.

--- lib/data/api_service.dart
+++ lib/data/api_service.dart
@@ -1,20 +1,32 @@
 import 'package:advanced_shopping_list_frontend/data/model/product_suggestion/product_suggestion.dart';
 import 'package:camera/camera.dart';
+import 'package:dio/dio.dart';
 
 abstract class ApiService {
-  Future<ProductSuggestion> getProductSuggestion(
-    String userId,
-    XFile file,
-  );
+  Future<ProductSuggestion> getProductSuggestion(String userId, XFile file);
 }
 
 class ApiServiceImpl implements ApiService {
+  final Dio dio;
+
+  const ApiServiceImpl(this.dio);
+
   @override
   Future<ProductSuggestion> getProductSuggestion(
     String userId,
-      XFile file,
-  ) {
-    // TODO: implement getProductSuggestion
-    throw UnimplementedError();
+    XFile file,
+  ) async {
+    String fileName = file.path.split("/").last;
+    FormData formData = FormData.fromMap({
+      "user_id": userId,
+      "file": await MultipartFile.fromFile(file.path, filename: fileName),
+    });
+
+    final response = await dio.post(
+      "/get_product_recommendation",
+      data: formData,
+    );
+
+    return response.data;
   }
 }
